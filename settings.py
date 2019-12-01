9154333cc00ec6cd6561d1721a1febf88c8a9812
Adriano Gomes <adrianogsss@gmail.com>
2019-12-01 12:44:58 -0300

    feat: alterada quantidade de neurônio na camada escondida

--- settings.py
+++ settings.py
@@ -5,15 +5,15 @@ quanto_em_quanto_faz_checkpoint = 250
 nome_arquivo_checkpoint = 'backup-rede-neuralzinha-'
 extensao_checkpoint = '.nn'
 
-le_de_arquivo = True
+le_de_arquivo = False
 
-somente_testa = True
+somente_testa = False
 
 momentum = 0.9
 
 taxa_aprendizagem = 0.1
 
-quantidade_neuronios_camada_escondida = 42
+quantidade_neuronios_camada_escondida = 40
 quantidade_neuronios_camada_saida = 36
 quantidade_neuronios_camada_entrada = 48
 
