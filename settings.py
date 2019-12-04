ec85b3a46bd7e1a3d6777594d73406beae7102f2
Adriano Gomes <adrianogsss@gmail.com>
2019-12-03 23:28:01 -0300

    melhor definição de pasta de backup

--- settings.py
+++ settings.py
@@ -5,8 +5,10 @@ quanto_em_quanto_faz_checkpoint = 250
 nome_arquivo_checkpoint = 'backup-rede-neuralzinha-'
 extensao_checkpoint = '.nn'
 
+pasta_arquivos_backups = './backups/'
 nome_arquivo_backup_pesos_sem_extensao = 'backup-rede-neuralzinha-2248-lr-01'
-nome_arquivo_backup_pesos = './backups/old_backups/' + nome_arquivo_backup_pesos_sem_extensao + '.nn'
+nome_arquivo_backup_pesos = pasta_arquivos_backups + 'old_backups/' + nome_arquivo_backup_pesos_sem_extensao + '.nn'
+
 
 le_de_arquivo = True
 
@@ -22,3 +24,6 @@ quantidade_neuronios_camada_entrada = 48
 
 def remove_ponto(string):
     return string.replace('.','')
+
+def gera_nome_arquivo_backup(i):
+    return pasta_arquivos_backups + nome_arquivo_checkpoint  + str(i) + '-lr-' + remove_ponto(str(taxa_aprendizagem)) + '-momentum-' + remove_ponto(str(momentum)) + '-neurons-' + str(quantidade_neuronios_camada_escondida) + extensao_checkpoint
